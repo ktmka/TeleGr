68b237754a9c771f213831e5d8010e2b225b4c3b
ktmka <tomatimoto@gmail.com>
2023-02-20 21:21:19 +0500

    update tmrph_bot

--- Telegram_bot1/tmrph_bot.py
+++ Telegram_bot1/tmrph_bot.py
@@ -1,5 +1,7 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler
+import games
+file_input = "DBase.txt"
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
@@ -12,10 +14,26 @@ async def sum(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     y = int(items[2])
     await update.message.reply_text(f"сумма {x} + {y} = {x+y}")
 
+async def play(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    msg = update.message.text.split()
+    await update.message.reply_text(games.show_matrix())
+    await update.message.reply_text(msg)
+    await update.message.reply_text(games.player(int(msg[1])))
+
+    
+async def joke(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    msg = update.message.text.split()
+    await update.message.reply_text(games.random_string())
+    # await update.message.reply_text(msg)
 
 app = ApplicationBuilder().token("5902874803:AAGO4WkdEEnXy8FGF14tVTneQVNFERcHeaE").build()
+print("start server")
 
 app.add_handler(CommandHandler("hello", hello))
 app.add_handler(CommandHandler("sum", sum))
+app.add_handler(CommandHandler("play", play))
+app.add_handler(CommandHandler("joke", joke))
+app.add_handler(MessageHandler(any, any))
+
 
 app.run_polling()
